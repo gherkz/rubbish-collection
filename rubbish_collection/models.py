b81aec7a9f892594d64b28707a8f901130c7677c
Jack Harrison <jack@jack-harrison.co.uk>
2025-04-05 10:48:27 +0000

    Add models

--- rubbish_collection/models.py
+++ rubbish_collection/models.py
@@ -1,3 +1,40 @@
-# from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import Model, UniqueConstraint
+
+
+class Address(Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    street = models.TextField()
+    city = models.TextField()
+    postcode = models.TextField()
+    council = models.TextField()
+
+    class Meta:
+        db_table = 'app_version'
+        constraints = [
+            UniqueConstraint(fields=["street", "city", "postcode"],
+                             name="unique_street_city_postcode")
+        ]
+
+
+class RubbishType(Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+
+
+class CollectionSchedule(Model):
+    class CollectionFrequency(models.TextChoices):
+        WEEKLY = 'W', 'Weekly'
+        BIWEEKLY = 'B', 'Bi-Weekly'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    rubbish_type = models.ForeignKey(RubbishType, on_delete=models.RESTRICT)
+    frequency = models.CharField(
+        max_length=1,
+        choices=CollectionFrequency.choices,
+    )
+    start_date = models.DateField()
+    import_date = models.DateField()
